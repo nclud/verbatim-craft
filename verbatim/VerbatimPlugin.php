79d4747c71b39d21ca3dfbe66091b6f7e79c70c5
aberkie <berkowitz@nclud.com>
2015-02-14 16:08:28 -0500

    Updating default settings

--- verbatim/VerbatimPlugin.php
+++ verbatim/VerbatimPlugin.php
@@ -32,19 +32,17 @@ class VerbatimPlugin extends BasePlugin
 	protected function defineSettings()
 	{
 		return array(
+			
+			'highlightParent' => array(AttributeType::Mixed, 'default' => 'true'),
 			'searchContainer' => array(AttributeType::Mixed, 'default' => 'body'),
-			'highlightParent' => array(AttributeType::Mixed, 'default' => "true"),
-			'defaultStyling' => array(AttributeType::Mixed, 'default' => "true"),
-			'animated' => array(AttributeType::Mixed, 'default' => "true"),
-			'allowImages' => array(AttributeType::Mixed, 'default' => "true"),
-			'animationSpeed' => array(AttributeType::Number, 'default' => 2000),
-			'scrollingOffset' => array(AttributeType::Number, 'default' => 200),
-			'addedClass' => array(AttributeType::Mixed, 'default' => 'verbatim-found-content'),
 			'highlightedClass' => array(AttributeType::Mixed, 'default' => 'highlight'),
 			'highlightColor' => array(AttributeType::Mixed, 'default' => 'rgb(255,255,0)'),
 			'selectedClass' => array(AttributeType::Mixed, 'default' => 'verbatim-selected-text'),
 			'buttonClass' => array(AttributeType::Mixed, 'default' => 'verbatim-button-container'),
-			'bitlyToken' => array(AttributeType::Mixed, 'default' => null)
+			'animated' => array(AttributeType::Mixed, 'default' => "true"),
+			'animationSpeed' => array(AttributeType::Number, 'default' => 2000),
+			'scrollingOffset' => array(AttributeType::Number, 'default' => 200),
+			'allowImages' => array(AttributeType::Mixed, 'default' => "true")
 		);
 	}
 
